40d04e8c519d05f736ec8612b9ff42376a5e4249
Rudy Hardeman <zarya@gigafreak.net>
2011-05-24 20:39:34 +0200

    Moar updates

--- bank.py
+++ bank.py
@@ -4,6 +4,7 @@ class Bank():
         self.member = 0
         self.username = "Mr/Ms Guest"
         self.total = float(0.00)
+        self.products = []
 
     def account(self):
         if self.member == 0:
@@ -35,6 +36,35 @@ class Bank():
         cursor.execute("""UPDATE member SET balance = %s WHERE nick = %s LIMIT 1""", (self.balance,self.username))
         print "200: Your balance is %s" % self.balance
 
+    def product_add(self,barcode):
+        cursor = self.db.cursor()
+        if self.member == 1:
+            cursor.execute("""SELECT name,member_price FROM products WHERE barcode = %s LIMIT 1""" , (barcode,))
+        else:
+            cursor.execute("""SELECT name,price FROM products WHERE barcode = %s LIMIT 1""" , (barcode,))
+
+        if cursor.rowcount == 0:
+            print "Not found"
+            return 
+
+        result = cursor.fetchone()
+        self.total = self.total + float(result[1])
+        self.products.append([result[0],result[1],barcode])
+        for product in self.products:
+            print "\t%s \t#%s\t DROP: %s Euro" % (self.username,product[0],product[1])
+        print "\t\t\t\tSubtotal: %s" % self.total
+
+    def pay(self):
+        print "%s payed %s euro to the register" % (self.username,self.total)
+        try:
+            self.total = float(0.00)
+        except:
+            pass
+        if self.member == 1:
+            self.logout()
+        self.products = []
+
+
     def login(self,barcode):
         cursor = self.db.cursor()
         cursor.execute("""SELECT nick,balance FROM member WHERE barcode = %s LIMIT 1""" , (barcode,))
@@ -56,8 +86,14 @@ class Bank():
         self.member = 0
         self.username = "Mr/Ms Guest"
         print "User logged out"
+        if self.total > 0.00:
+            self.reset()
 
     def reset(self):
-        self.total = float(0.00)
+        try:
+            self.total = float(0.00)
+        except:
+            pass
+        self.products = []
         print "Transaction aborted"
         
--- bitbank.py
+++ bitbank.py
@@ -3,25 +3,6 @@ import ConfigParser
 import sys
 from bank import Bank
 
-def reset():
-    global member,total,username
-    member = 0
-    try:
-        del total
-    except:
-        pass
-    try:
-        del balance
-    except:
-        pass
-    username = "Mr/Ms Guest"
-
-def deposit(username,amount):
-    global db
-    cursor = db.cursor()
-    balance = balance + float(amount)
-    print "Your balance is: %s" % balance
-    cursor.execute("""UPDATE member SET balance = %s WHERE nick = %s LIMIT 1""", (balance,username))
 
 def run():
     _Runner = True
@@ -35,19 +16,22 @@ def run():
         db=config.get('Database', 'database'))
  
     bank = Bank(db)
+    defaultusername = bank.username
     while _Runner == True:
-        barcode=raw_input('Please scan [usercard,product barcode]: ')
+        if bank.username != defaultusername:
+            barcode=raw_input('%s please scan [product barcode]: ' % bank.username)
+        else:
+            barcode=raw_input('Please scan [usercard,product barcode]: ')
 
         if barcode.startswith('1337'):
             bank.login(barcode)
 
-        elif barcode == "clear":
+        elif barcode == "clear" or barcode == "abort":
             print "Aborted not payed"
-            reset()
+            bank.reset()
 
         elif barcode == "pay":
-            print "Payed %s " % total
-            reset()
+            bank.pay()
 
         elif barcode == "logout":
             bank.logout()
@@ -69,23 +53,7 @@ def run():
             _Runner = False
         
         else:
-            cursor = db.cursor()
-            if bank.member == 1:
-                cursor.execute("""SELECT name,member_price FROM products WHERE barcode = %s LIMIT 1""" , (barcode,))
-            else:
-                cursor.execute("""SELECT name,price FROM products WHERE barcode = %s LIMIT 1""" , (barcode,))
-    
-            if cursor.rowcount == 0:
-                print "Not found"
-                continue
-
-            result = cursor.fetchone()
-            print "\t\t %s \t\t DROP: %s # %s" % (username,result[1],result[0])
-            try:
-                bank.total = bank.total + result[1]
-            except:
-                bank.total = result[1]
-            print "Subtotal: %s" % total
+            bank.product_add(barcode)
 
 if __name__ ==  '__main__':
     sys.exit(run())
